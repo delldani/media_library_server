430695955a2249119b10e64aa7027e0bda37ae26
gordongecco <delldani001@gmail.com>
2019-12-11 15:18:47 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+fs.mkdirSync("./aron");
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use(express.static("dani"));
