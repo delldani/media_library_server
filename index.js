a4a1e2d07f14561457cf4585efbd76b05627073b
gordongecco <gordongecco77@gmail.com>
2019-12-18 16:41:13 +0100

    12.18

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ const app = express();
 const port = 3000;
 
 // fs.mkdirSync("./aron");
-// app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+  //     res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //     next();
+  //   });
+  
+  let corsOptions = {
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+  app.use(cors(corsOptions));
+ 
+  var multer  = require('multer');
+var upload = multer({ dest: 'dani/' });
+ 
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+ 
+app.post('/', upload.any(), (req, res) =>{
+  console.log(req.file);
+  res.send('mentve!')});
+ 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.use(express.static("dani"));
