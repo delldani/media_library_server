13bb2b0062ba9b39c5ee315f7ebc712fb4caa0bb
gordongecco <gordongecco77@gmail.com>
2019-12-18 11:15:38 +0100

    12.18

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 // fs.mkdirSync("./aron");
+// app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
