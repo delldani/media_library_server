60dff27d45406eef3b12cc049abd46fa3da739df
gordongecco <gordongecco77@gmail.com>
2019-12-19 11:33:00 +0100

    12.19

--- index.js
+++ index.js
@@ -4,17 +4,9 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// fs.mkdirSync("./aron");
-
-// app.use(function(req, res, next) {
-  //     res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-  //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //     next();
-  //   });
-  
   let corsOptions = {
     origin: 'http://localhost:3001',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200 
   }
   app.use(cors(corsOptions));
  
@@ -24,9 +16,8 @@ var upload = multer({ dest: 'dani/' });
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
- 
-app.post('/', upload.any(), (req, res) =>{
-  console.log(req.file);
+
+app.post('/', upload.single('avatar'), (req, res) =>{
   res.send('mentve!')});
  
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
