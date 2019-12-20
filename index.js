79f03084f9953463e9e029f157d86271c76fa715
gordongecco <gordongecco77@gmail.com>
2019-12-20 10:32:52 +0100

    12.20

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
 const express = require("express");
 const fs = require("fs");
-const cors = require('cors');
+const cors = require("cors");
+var multer = require("multer");
+
 const app = express();
 const port = 3000;
 
-  let corsOptions = {
-    origin: 'http://localhost:3001',
-    optionsSuccessStatus: 200 
-  }
-  app.use(cors(corsOptions));
- 
-  var multer  = require('multer');
+let corsOptions = {
+  origin: "http://localhost:3001",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-  var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'dani/')
+function multerMaker(path) {
+  let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, path);
     },
-    filename: function (req, file, cb) {
-      cb(null, 'kép'+ '-' + Date.now()+'.jpg')
+    filename: function(req, file, cb) {
+      cb(null, "kép" + "-" + Date.now() + ".jpg");
     }
   });
+  return multer({ storage: storage });
+}
 
-  var storagePictures = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'pictures/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, 'kép'+ '-' + Date.now()+'.jpg')
-    }
-  });
-
-var upload = multer({ storage: storage });
-var uploadPictures = multer({ storage:storagePictures});
-// var uploadPictures = multer({ dest: 'pictures/' });
- 
+let upload = multerMaker("dani/");
+let uploadPictures = multerMaker("pictures/");
 
 app.get("/", (req, res) => res.send("response Ok !"));
 
-
-app.post('/', upload.single('avatar'), (req, res) =>{
+app.post("/", upload.single("avatar"), (req, res) => {
   // console.log(req.files);
   // console.log(req.body);
-  res.send('mentve!')});
+  res.send("mentve!");
+});
 
-  app.post('/pictures', uploadPictures.array('avatar',11), (req, res) =>{
-    console.log(req.files);
-    // console.log(req.body);
-    res.send('mentve képek!')});
- 
+app.post("/pictures", uploadPictures.array("avatar"), (req, res) => {
+  console.log(req.files);
+  // console.log(req.body);
+  res.send("mentve képek!");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.use(express.static("dani"));
